a32824d560049f12bcfb7d84eb820b4c03242116
EnzoJDeMarchi <edemarchi094@gmail.com>
2024-04-20 23:32:02 -0300

    control enzo

---
+++ Assets/Enzo/movetoward_enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movetoward_enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject virus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
