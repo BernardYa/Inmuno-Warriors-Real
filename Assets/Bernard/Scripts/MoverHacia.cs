05e143546e630863ff2005d6759fbd49fc4243bb
BernardYa <128098828+BernardYa@users.noreply.github.com>
2024-04-23 19:17:42 -0300

    Presentamos esto con mi pantalla

---
+++ Assets/Bernard/Scripts/MoverHacia.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class MoverHaciaDestino : MonoBehaviour
+{
+    [SerializeField] float velocidad;
+    [SerializeField] Transform destino;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destino.position, velocidad);
+    }
+}
